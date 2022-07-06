a61f453bc0488136a1549536602b62913069b747
Tien Le <leviettien1808@gmail.com>
2022-07-06 18:56:37 +0700

    Create method get

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const products = require('./data')
 
 // Init server
 const app = express();
 
 // Conver to JSON
 app.use(express.json());
+
+app.get('/api/products', (req, res) => {
+  res.send(products)
+})
 
 app.listen(4000, () => console.log('server running'));
